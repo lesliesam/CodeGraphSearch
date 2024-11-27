a1eab13b44aa2ec34734611f2370265f67212450
Sam Yu <samzyu@amazon.com>
2024-11-27 10:26:06 +0800

    Initial commit

---
+++ test/code_graph_search.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CodeGraphSearch from '../lib/code_graph_search-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/code_graph_search-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CodeGraphSearch.CodeGraphSearchStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
